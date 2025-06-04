0fbad96237acc742ed68a102e77ea937c46aa615
amogus <110690851+vovan-2007@users.noreply.github.com>
2025-06-04 12:27:48 +0400

    Add files via upload

---
+++ lab7t16/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace lab7t16
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4, m = 5;
+            int[,] matrix = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(1, 10);
+                }
+            }
+            double threshold = 5.0;
+            int count = 0;
+            for (int i = 0; i < n; i++)
+            {
+                double sum = 0;
+                for (int j = 0; j < m; j++)
+                {
+                    sum += matrix[i, j];
+                }
+                double avg = sum / m;
+                if (avg < threshold)
+                {
+                    count++;
+                }
+            }
+            Console.WriteLine($"Количество строк со средним арифметическим меньше {threshold}: {count}");
+            Console.WriteLine("Нажмите любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ lab7t17/Program.cs
@@ -0,0 +1,56 @@
+﻿namespace lab7t17
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int n = 4, m = 5;
+            int[,] matrix = new int[n, m];
+            Random rnd = new Random();
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    matrix[i, j] = rnd.Next(0, 3);
+                }
+            }
+            int lastRow = n - 1;
+            bool Nuli = true;
+            for (int j = 0; j < m; j++)
+            {
+                if (matrix[lastRow, j] != 0)
+                {
+                    Nuli = false;
+                    break;
+                }
+            }
+            if (Nuli && n > 1)
+            {
+                lastRow = n - 2;
+            }
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    if (matrix[i, j] == 0 && i != lastRow)
+                    {
+                        int temp = matrix[i, j];
+                        matrix[i, j] = matrix[lastRow, j];
+                        matrix[lastRow, j] = temp;
+                    }
+                }
+            }
+            Console.WriteLine("Матрица после обмена:");
+            for (int i = 0; i < n; i++)
+            {
+                for (int j = 0; j < m; j++)
+                {
+                    Console.Write(matrix[i, j] + " ");
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine("Нажмите на любую клавишу для выхода...");
+            Console.ReadKey();
+        }
+    }
+}
